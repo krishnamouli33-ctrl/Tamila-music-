7fd599e2c2b5d887b27e5902d7af4d37baff34e5
krishnamouli33-ctrl <krishnamouli33@gmail.com>
2026-07-22 09:50:24 +0530

    Create build. yml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,8 +45,7 @@ android {
       isMinifyEnabled = false
       proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
       signingConfig = signingConfigs.getByName("release")
-    }
-    debug { signingConfig = signingConfigs.getByName("debugConfig") }
+    } { }
   }
   compileOptions {
     sourceCompatibility = JavaVersion.VERSION_11
